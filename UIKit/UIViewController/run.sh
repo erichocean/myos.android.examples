d82806f2b122388b14825ed56a9cc745bdbdecb5
Amr Aboelela <amraboelela@gmail.com>
2014-09-23 16:22:13 -0700

    Updated run.sh files

---
+++ UIKit/UIViewController/run.sh
@@ -0,0 +1,9 @@
+#
+# Copyright 2014 myOS Group. All rights reserved.
+#
+
+source build.sh
+
+EXAMPLE_DIRECTORY=$(pwd | awk -F'/' '{print $NF}')
+cd ${MYOS_PATH}/android/examples/UIKit/${EXAMPLE_DIRECTORY}
+source ${MYOS_PATH}/android/sdk/scripts/nativeActivity-run.sh
